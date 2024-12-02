42d9db0007ceb8c0db0713b068f2b67d9a77ad27
EC2 Default User <ec2-user@ip-172-31-15-4.ap-south-1.compute.internal>
2024-12-02 01:11:05 +0000

    Clean up

---
+++ admin/application/logs/log-2024-12-01.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-12-01 13:43:55 --> Could not find the language line "add_more"
+ERROR - 2024-12-01 13:44:19 --> Could not find the language line "add_more"
+ERROR - 2024-12-01 13:44:46 --> Could not find the language line "add_more"
+ERROR - 2024-12-01 13:44:47 --> Could not find the language line "add_more"
+ERROR - 2024-12-01 13:47:02 --> Could not find the language line "add_more"
+ERROR - 2024-12-01 13:47:31 --> Could not find the language line "add_more"
