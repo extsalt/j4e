6a3ac0b21a80b8a8e88db0c19cf49e0fdde7feb0
= <=>
2025-01-18 13:51:34 +0000

    Enhance user query functionality and return response as an array

--- application/controllers/Api/UserController.php
+++ application/controllers/Api/UserController.php
@@ -1,4 +1,7 @@
 <?php
+
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class UserController extends CI_Controller
@@ -12,22 +15,52 @@ class UserController extends CI_Controller
     public function index()
     {
         $query = $_GET['query'];
-        $sql = "SELECT * FROM user";
+        $location = $_GET['location'];
+        $service = $_GET['service'];
+        $industry = $_GET['industry'];
+        $sql = "SELECT `id`,`first_name`,`middle_name`,`last_name`,`phone`,`designation`,`company`,`avatar`,`company_address`,
+                `business_category`,`membership_type`,`target_audiance`,`business_entity`,`business_experties`,`business_type` FROM user WHERE user_type = '1' AND membership_type IN (2, 1) ";
         if ($query) {
-            $sql .= " where first_name LIKE '%$query%' OR last_name LIKE '%$query%' ";
+            $sql .= " AND `first_name` LIKE '%$query%' OR `middle_name` LIKE '%query%' OR `last_name` LIKE '%$query%' ";
+        }
+        if ($location) {
+            $sql .= " AND `company_address` LIKE '%$location%' ";
+        }
+        if ($industry) {
+            $sql .= " AND `business_type` LIKE '%$industry%' ";
+        }
+        if ($service) {
+            $sql .= " AND `business_experties` LIKE '%$service%' ";
         }
         $sql .= " LIMIT 20";
-        $results = $this->db->query($sql)->result();
-        //var_dump($results);
-        //die;
-        foreach ($results as &$result) {
-            if ($result->membership_type == '2') {
-                $result->membership_type = 'Registered';
+        //return response as array rather than standard object
+
+        $results = $this->db->query($sql)->result_array();
+        $response = [];
+        foreach ($results as $result) {
+            $member = [];
+            $member['id'] = $result['id'];
+            $member['firstName'] = $result['first_name'] ?? '';
+            $member['middleName'] = $result['middle_name'] ?? '';
+            $member['lastName'] = $result['last_name'] ?? '';
+            $member['phone'] = $result['phone'] ?? '';
+            $member['designation'] = $result['designation'] ?? '';
+            $member['company'] = $result['company'] ?? '';
+            $member['avatar'] = $result['avatar'] ?? '';
+            $member['companyAddress'] = $result['company_address'] ?? '';
+            $member['businessCategory'] = $result['business_category'] ?? '';
+            $member['targetAudience'] = $result['target_audience'] ?? '';
+            $member['businessEntity'] = $result['business_entity'] ?? '';
+            $member['businessExpertise'] = $result['business_experise'] ?? '';
+            $member['businessType'] = $result['business_type'] ?? '';
+            if ($result['membership_type'] == '2') {
+                $member['membershipType'] = 'Registered';
             } else {
-                $result->membership_type = 'Visitor Member';
+                $member['membershipType'] = 'Visitor Member';
             }
+            $response[] = $member;
         }
-        echo @json_encode($results);
+        echo @json_encode($response);
     }
 
     public function getMemberById()
