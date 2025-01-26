09c79efc382336a04d3376c6d86ec38900c757a9
Ganesh Gupta <45989209+extsalt@users.noreply.github.com>
2025-01-26 11:25:04 +0000

    Add refund policy page and update routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -55,3 +55,4 @@ $route['reviews/(:any)'] = 'home/reviews/$1';
 
 $route['terms_conditions'] = 'home/terms_conditions';
 $route['privacy_policy'] = 'home/privacy_policy';
+$route['refund_policy'] = 'home/refund_policy';
---
+++ application/views/refund_policy.php
@@ -0,0 +1,17 @@
+<?php include 'header.php'; ?>
+<?php include 'menu1.php'; ?>
+
+    <section class=" login-reg">
+    <div class="container">
+        <div class="row">
+            <div class="plainpg">
+                <h1>Privacy Policy</h1>
+              <p>
+              Just 4 Us believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy: Cancellations will be considered only if the request is made within 16-30 days of placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them. Just 4 Us does not accept cancellation requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good. In case of receipt of damaged or defective items please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. This should be reported within 16-30 days of receipt of the products. In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 16-30 days of receiving the product. The Customer Service Team after looking into your complaint will take an appropriate decision. In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them. In case of any Refunds approved by the Just 4 Us, it’ll take 1-2 days for the refund to be processed to the end customer.
+              </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+   <?php include 'footer.php'; ?>     
