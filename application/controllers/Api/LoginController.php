1c3077b2367dd0afe149b5a339004a629cac1f6c
Ganesh Gupta <45989209+extsalt@users.noreply.github.com>
2025-01-31 02:34:47 +0000

    Add user authentication controllers and model for user management

---
+++ application/controllers/Api/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class UserController extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        header('Content-Type: application/json');
+    }
+
+    public function login()
+    {
+        
+    }
+}
---
+++ application/models/User_Model.php
@@ -0,0 +1,52 @@
+<?php
+
+class User_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // Load the database library (if not autoloaded)
+        $this->load->database();
+    }
+
+    // Get all users from the 'users' table
+    public function get_all_users()
+    {
+        $query = $this->db->get('user');
+        return $query->result();
+    }
+
+    // Get a single user by ID from the 'users' table
+    public function get_user_by_id($id)
+    {
+        $query = $this->db->get_where('user', array('id' => $id));
+        return $query->row();
+    }
+
+    public function getUserByEmail($email)
+    {
+        $query = $this->db->get_where('user', array('email' => $email));
+        return $query->row_array();
+    }
+    
+    // Insert a new user into the 'users' table
+    public function insert_user($data)
+    {
+        $this->db->insert('user', $data);
+        return $this->db->insert_id(); // Return the ID of the inserted row
+    }
+
+    // Update a user in the 'users' table
+    public function update_user($id, $data)
+    {
+        $this->db->where('id', $id);
+        $this->db->update('user', $data);
+    }
+
+    // Delete a user from the 'users' table
+    public function delete_user($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('user');
+    }
+}
