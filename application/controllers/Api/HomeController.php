49fd0fbc54438d56458558bd3df5523f0588287e
Ganesh Gupta <45989209+extsalt@users.noreply.github.com>
2025-02-04 14:39:12 +0000

    .

--- application/controllers/Api/HomeController.php
+++ application/controllers/Api/HomeController.php
@@ -20,12 +20,14 @@ class HomeController extends CI_Controller
 
     public function index()
     {
-        $members = $this->db->query("SELECT id, first_name, last_name, company FROM user ORDER BY created_time DESC LIMIT 5")->result_array();
-        foreach ($members as &$member) {
+        $memberResults = $this->db->query("SELECT id, first_name, last_name, company FROM user ORDER BY created_time DESC LIMIT 5")->result_array();
+        $members = [];
+        foreach ($memberResults as &$memberRow) {
             $member['id'] = $member['id'];
             $member['firstName'] = ucfirst($member['first_name']) ?? '';
             $member['lastName'] = ucfirst($member['last_name']) ?? '';
             $member['company'] = ucfirst($member['company']) ?? '';
+            $members[] = $member;
         }
         $this->load->model('EventModel');
         $events = $this->EventModel->get_events_home();
