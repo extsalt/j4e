81e8d4357eaaed53675d1b5fdce3e49f1fb3826f
Ganesh Gupta <45989209+extsalt@users.noreply.github.com>
2025-02-09 05:36:10 +0000

    Fix timestamp format in mark_as_verified method in OTPPhoneModel

--- application/models/OTPPhoneModel.php
+++ application/models/OTPPhoneModel.php
@@ -28,7 +28,7 @@ class OTPPhoneModel extends CI_Model
     // Get a single user by ID from the 'users' table
     public function mark_as_verified($phone, $otp)
     {
-        return $this->db->update('otp_phone', ['verified_at' => date('Y-m-d H:is:')], array('phone' => $phone, 'otp' => $otp), '1');
+        return $this->db->update('otp_phone', ['verified_at' => date('Y-m-d H:i:s')], array('phone' => $phone, 'otp' => $otp), '1');
     }
 
     public function delete_by_phone_and_otp($phone, $otp)
