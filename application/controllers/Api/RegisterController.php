88fae1e8c5b62b1b473f3431a581b7a5b42c1c77
Ganesh Gupta <45989209+extsalt@users.noreply.github.com>
2025-02-10 02:02:01 +0000

    .

--- application/controllers/Api/RegisterController.php
+++ application/controllers/Api/RegisterController.php
@@ -58,7 +58,8 @@ class RegisterController extends CI_Controller
             $data['email'] = $insertData['email_address'];
             $data['token'] = $token;
             $data['avatar'] = 'https%3A%2F%2Fj4e.s3.ap-south-1.amazonaws.com%2Fpublic%2Fdefault.png';
-            $data['company'] = '';
+            $data['company'] = $insertData['company'];
+            $data['designation'] = $insertData['designation'];
             $tokenInsertData['token'] = $token;
             $tokenInsertData['user_id'] = $userInsertID;
             $this->load->model('ApiTokenModel');
